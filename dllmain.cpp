b648e014a04cd490c794e97c0d2367ca804bf0f0
SCell555 <kubci.rusnk645@gmail.com>
2025-03-07 18:03:15 +0100

    Do not link to anything in release

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,7 +1,11 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
+#ifdef _DEBUG
+#define _DllMainCRTStartup DllMain
+#endif
+
+BOOL APIENTRY _DllMainCRTStartup( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
 {
 	switch (ul_reason_for_call)
 	{
